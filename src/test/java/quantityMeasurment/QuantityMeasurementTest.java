4e4e1f3a029042311bc7d43505f7ca16d3740e6f
kumudgarg <kumud50garg@gmail.com>
2019-12-17 13:53:36 +0530

    ADDED code and Test for checking equality of Inch objects

--- src/test/java/quantityMeasurment/QuantityMeasurementTest.java
+++ src/test/java/quantityMeasurment/QuantityMeasurementTest.java
@@ -19,5 +19,25 @@ public class QuantityMeasurementTest {
         Assert.assertNotEquals(feet1, feet2);
     }
 
+    @Test
+    public void given1Feetand2FeetAsNull_ShouldReturnNotEquals() {
+        Feet feet1 = new Feet(0.0);
+        Feet feet2 = null;
+        boolean type = feet1.equals(feet2);
+        Assert.assertEquals(false,type);
+    }
 
+    @Test
+    public void given0InchAnd0Inch_ShouldreturnEqual() {
+        Inch inch1 = new Inch(0.0);
+        Inch inch2 = new Inch(0.0);
+        Assert.assertEquals(inch1,inch2);
+    }
+
+    @Test
+    public void given0InchAnd1Inch_ShouldreturnNotEqual() {
+        Inch inch1 = new Inch(0.0);
+        Inch inch2 = new Inch(1.0);
+        Assert.assertNotEquals(inch1,inch2);
+    }
 }
